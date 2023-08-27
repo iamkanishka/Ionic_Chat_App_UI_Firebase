c96e575ebfdddc847bb101718331d2910f10d1d1
iamkanishka <kanishkabc123@gmail.com>
2023-08-27 18:16:04 +0530

    Login and Signup Functinality Integrarted

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,27 +12,29 @@ import { Router } from '@angular/router';
 })
 export class LoginPage   {
 
-  form!: FormGroup;
+  loginForm!: FormGroup;
   isTypePassword: boolean = true;
+  isLoading:boolean=false
 
-  constructor(private readonly router: Router) {
+
+  constructor(private readonly router: Router, private formBuilder: FormBuilder,private authService:AuthService,private alertController:AlertController ) {
     this.initForm();
   }
 
 
 
   initForm() {
-    this.form = new FormGroup({
-      email: new FormControl('', 
-        {validators: [Validators.required, Validators.email]}
-      ),
-      password: new FormControl('', 
-        {validators: [Validators.required, Validators.minLength(8)]}
-      ),
+    this.loginForm =this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
 
-    console.log(this.form.get('password')?.errors);
+
     
+  }
+
+  get f(){
+   return this.loginForm.controls;
   }
 
   onChange() {
@@ -37,9 +42,42 @@ export class LoginPage   {
   }
 
   onSubmit() {
-    // if(!this.form.valid) return;
-    // console.log(this.form.value);
-    this.router.navigate(['/home'])
+    if(this.loginForm.invalid) return;
+   this.logUser();
+  }
+
+
+  logUser(){
+    this.isLoading =true;
+    this.authService.login(this.loginForm.value.email,this.loginForm.value.password).then((response:any)=>{
+    this.isLoading =false;
+     console.log(response);
+
+     this.router.navigate(['/home']);
+     this.showAlert('User Logged Successfully')
+     this.loginForm.reset();
+   }).catch(e=>{
+    console.log(e)
+    this.isLoading =false;
+
+    let msg : string =  `Couldn't Login, Please try Again`;
+    if(e.code==='auth/user-not-found')  msg = 'E-mail address could not be found';
+    else if(e.code==='auth/wrong-password')  msg = 'Please enter Correct Password';
+
+    this.showAlert(msg)
+
+   })
+  }
+
+  async showAlert(message:string) {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      //subHeader: 'Important message',
+      message: message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 
 }
